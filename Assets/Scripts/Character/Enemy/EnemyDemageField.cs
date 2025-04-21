3694483a9c3af6f99048669fd0d885cee32e3c8f
fiore <cjsdlf44@gmail.com>
2025-04-21 16:24:23 +0900

    DEG-41 전투 패턴 구현 시작

---
+++ Assets/Scripts/Character/Enemy/EnemyDemageField.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class EnemyDemageField : MonoBehaviour
+{
+    private float damage;
+    
+}
--- Assets/Scripts/Character/Enemy/EnemyState/EnemyStateAttack.cs
+++ Assets/Scripts/Character/Enemy/EnemyState/EnemyStateAttack.cs
@@ -36,20 +36,19 @@ public class EnemyStateAttack : IEnemyState
 
     private IEnumerator VerticalAttackSequence()
     {
-        // 1. 전조 이펙트 생성
+        yield return new WaitForSeconds(0.1f);
+        // 1. 전조 이펙트 생성, 검을 들어올림
         _enemyAttackController.TriggerRandomWarning(_enemyController.transform.position, _enemyController.transform.rotation);
-        // 2. 검을 들어올림
-        yield return new WaitForSeconds(3f);
+        // 2. 대기(전조와 검 들어올리는 애니메이션을 위함)
+        yield return new WaitForSeconds(2f);
 
-        // 3. 대기(전조와 검 들어올리는 애니메이션을 위함)
-
-        // 4. 검 휘두르기
+        // 3. 검 휘두르기
         _animator.SetTrigger(VertiSlash);
         _enemyAttackController.DestroyWarningArea();
-        // TODO : 5. 공격 판정 발생
+        // TODO : 4. 공격 판정 발생
 
         yield return new WaitForSeconds(1f);
-        // 6. 애니메이션 트리거 종료 -> 애니메이터 상태 머신으로 처리
+        // 5. 애니메이션 트리거 종료 -> 애니메이터 상태 머신으로 처리
         _enemyController.SetState(EnemyState.Trace);
     }
 
