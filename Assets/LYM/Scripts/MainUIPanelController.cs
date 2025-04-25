ae61935b3714fbbdc7cd590d165c19094f5c2315
Lim0_C <dladudcks22@naver.com>
2025-04-25 10:51:39 +0900

    DEG-85 [Feat] 메인화면UI 구현
    메인화면UI 구현 및 DOtween 추가

---
+++ Assets/LYM/Scripts/MainUIPanelController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIPanelController : MonoBehaviour
+{
+
+    public void OnClickStartButton()
+    {
+        
+    }
+
+    public void OnClickSettingsButton()
+    {
+        
+    }
+}
