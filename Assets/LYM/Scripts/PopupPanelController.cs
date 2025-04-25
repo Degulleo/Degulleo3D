c08a1d6656a4d0ac40a5d5fc11d050b45e91aefd
Lim0_C <dladudcks22@naver.com>
2025-04-25 17:54:31 +0900

    DEG-85 [Feat] 팝업패널 추가

---
+++ Assets/LYM/Scripts/PopupPanelController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupPanelController : PanelController
+{
+    [SerializeField] private GameObject confirmButton;
+    [SerializeField] private GameObject contradictButton;
+
+    public delegate void OnConfirmDelegate();
+    private OnConfirmDelegate _onConfirmDelegate;
+    public delegate void OnContradictDelegate();
+    private OnContradictDelegate _onContradictDelegate;
+    private void Start()
+    {
+        Show(false, "", () => {});
+    }
+
+    public void Show(bool isNeed2Contradict, string message, OnConfirmDelegate onConfirm)
+    {
+        confirmButton.SetActive(isNeed2Contradict);
+        _onConfirmDelegate = onConfirm;
+        base.Show();
+    }
+
+    public void Show(bool isNeed2Contradict, string message, OnConfirmDelegate onConfirm, OnContradictDelegate onContradict)
+    {
+        confirmButton.SetActive(isNeed2Contradict);
+        _onConfirmDelegate = onConfirm;
+        _onContradictDelegate = onContradict;
+        base.Show();
+    }
+
+    public void OnClickConfirm()
+    {
+        _onConfirmDelegate?.Invoke();
+        base.Hide();
+    }
+
+    public void OnClickContradict()
+    {
+        _onContradictDelegate?.Invoke();
+        base.Hide();
+    }
+}
