2708df17df65fa28be53a9ab9a081c2a16334989
Fiore <yukihira91@proton.me>
2025-04-29 16:06:07 +0900

    [feat] 랜덤 패턴 수행을 위한 메서드 추가
    Work in JIRA ISSUE DEG-100

--- Assets/Scripts/Character/Enemy/CasterDemonController.cs
+++ Assets/Scripts/Character/Enemy/CasterDemonController.cs
@@ -21,7 +21,38 @@ public class CasterDemonController : EnemyController
 
             // TODO : 배틀 중일 때 루프
             Debug.Log("## 몬스터의 교전 행동 루프");
-            StartCoroutine(ShotMagicMissile());
+            Thinking();
+        }
+    }
+
+
+    private void Thinking()
+    {
+        int selectedPattern = Random.Range(0, 10);
+
+        switch (selectedPattern)
+        {
+            case 0:
+
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+
+            case 8:
+
+            case 9:
+                StartCoroutine(ShotMagicMissile());
+                break;
         }
     }
 
